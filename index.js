ff019f083dccff54580f6ca0e5400c46b2c2f297
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-22 09:06:55 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordLetters = word.split('');
+  const backwordLetters = [];
+  for (let i = 0; i < wordLetters.length; i++) {
+    backwordLetters.unshift(wordLetters[i]);
+  }
+  const backword = backwordLetters.join('');
+  if (word === backword) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  Transform input (word) into an array of the letters (wordLetters).
+  Using a for loop, create a new array (backwordLetters) that orients those letters backwords.
+  Transform the backword array into a string (backword).
+  Compare 'word' to 'backword' and return true if identical and false if not.
 */
 
 /*
-  Add written explanation of your solution here
+  Create wordLetters as an array of word by using .split method.
+  Create empty array backwordLetters.
+  Use for loop to .unshift the letters from wordLetters array onto backwordLetters
+  Create backword by using .join method on backwordLetters.
+  Use If Else statement to compare word to backword.
+  If identical, return true; if not, return false.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("poop"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hello"));
+
+  console.log("");
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
